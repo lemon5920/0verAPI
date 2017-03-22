7a91f72eee6847cbbfa58004eba0406d2a947628
zxp86021 <zxp86021@gmail.com>
2017-03-22 23:25:46 +0800

    新增語言表格

---
+++ database/migrations/2016_08_01_052344_create_locales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locales', function (Blueprint $table) {
+            $table->string('lang_code')->primary();
+            $table->string('title')->unique();
+            $table->string('local_title')->unique();
+            $table->string('created_at');
+            $table->string('updated_at');
+            $table->string('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locales');
+    }
+}
