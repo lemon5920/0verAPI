2d7f67c70354b90b38de5646418b7ab520c71397
sunnyworm <sunnyworm@gmail.com>
2017-03-30 17:50:22 +0800

    修正 system_types 的檔名與 class 名稱, 修正 down() 移除的 table 名稱

--- database/migrations/2017_03_30_061931_create_system_types_table.php
+++ database/migrations/2017_03_30_061931_create_system_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSystemsTypeTable extends Migration
+class CreateSystemTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateSystemsTypeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('systems');
+        Schema::dropIfExists('system_types');
     }
 }
