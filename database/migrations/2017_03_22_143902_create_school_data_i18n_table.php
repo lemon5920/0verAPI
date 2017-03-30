1d3d3e53659b8db83cd159e9d7f5731133ced1a0
zxp86021 <zxp86021@gmail.com>
2017-03-30 14:51:50 +0800

    修正拼字錯誤 移動欄位到 school_data_i18n

--- database/migrations/2017_03_22_143902_create_school_data_i18n_table.php
+++ database/migrations/2017_03_22_143902_create_school_data_i18n_table.php
@@ -18,6 +18,10 @@ class CreateSchoolDataI18nTable extends Migration
             $table->string('lang');
             $table->string('name')->unique();
             $table->string('address')->unique();
+            $table->string('organization')->comment('學校負責僑生事務的承辦單位');
+            $table->string('dorm_info')->comment('宿舍說明');
+            $table->string('scholarship_info')->comment('獎學金說明');
+            $table->string('scholarship_dept')->comment('獎學金負責單位');
             $table->string('created_at');
             $table->string('updated_at');
             $table->string('deleted_at')->nullable();
