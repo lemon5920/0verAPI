a1f74a7e9ab2c2dec0a6ad72c0cdae356d36932d
sunnyworm <sunnyworm@gmail.com>
2017-03-30 16:30:22 +0800

    新增 school_types 結構

---
+++ database/migrations/2017_03_30_061931_create_systems_type_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemsTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_types', function (Blueprint $table) {
+            $table->string('systems')->comment('目前可以收學生的學制');
+            $table->string('created_at');
+            $table->string('updated_at');
+            $table->string('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('systems');
+    }
+}
