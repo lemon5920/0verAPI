8f41b7bcbc0e2ccdb08d99a1c5b5179c320d5ebb
sunnyworm <sunnyworm@gmail.com>
2017-03-30 14:35:30 +0800

    補上 school_data 的註解，將學制資料移到 systems 與 system_data

--- database/migrations/2017_03_22_143737_create_school_data_table.php
+++ database/migrations/2017_03_22_143737_create_school_data_table.php
@@ -14,42 +14,22 @@ class CreateSchoolDataTable extends Migration
     public function up()
     {
         Schema::create('school_data', function (Blueprint $table) {
-            $table->integer('code')->primary();
-            $table->string('type');
-            $table->string('phone');
-            $table->string('fax');
-            $table->string('homepage');
-            $table->string('organization')->comment('負責僑生事務的單位');
-            $table->string('undergraduate_memo');
-            $table->string('master_memo');
-            $table->string('associate_memo');
-            $table->integer('overseas_bs_limit');
-            $table->integer('overseas_master_limit');
-            $table->integer('overseas_phd_limit');
-            $table->integer('bs_not_reach');
-            $table->integer('master_not_reach');
-            $table->integer('phd_not_reach');
-            $table->integer('bs_add');
-            $table->integer('master_add');
-            $table->integer('phd_add');
-            $table->integer('i11l_bs_limit');
-            $table->integer('i11l_master_limit');
-            $table->integer('i11l_phd_limit');
-            $table->integer('sort_order');
-            $table->boolean('five_year_allowed');
-            $table->boolean('five_year_prepare');
-            $table->integer('five_year_confirmed_by');
-            $table->string('five_year_rule');
-            $table->string('approve_no');
-            $table->integer('self_limit');
-            $table->boolean('two_year_tech');
-            $table->string('dorm_info');
-            $table->string('scholarship_info');
-            $table->string('scholarship_dept');
-            $table->string('scholarship_url');
-            $table->string('created_at');
-            $table->string('updated_at');
-            $table->string('deleted_at')->nullable();
+            $table->integer('code')->primary()->commect('學校代碼');
+            $table->string('type')->commect('「公、私立」與「大學、科大」之組合＋「僑先部」共五種');
+            $table->string('phone')->commect('學校聯絡電話（+886-49-2910960#1234）');
+            $table->string('fax')->commect('學校聯絡電話（+886-49-2910960#1234）');
+            $table->string('homepage')->commect('學校網站網址');
+            $table->integer('sort_order')->commect('學校顯示排序（教育部給）');
+            $table->boolean('five_year_allowed')->commect('[中五]我可以招呢');
+            $table->boolean('five_year_prepare')->commect('[中五]我準備招了喔');
+            $table->integer('five_year_confirmed_by')->commect('[中五](school code)');
+            $table->string('five_year_rule')->commect('[中五]給海聯看的學則');
+            $table->string('approve_no')->commect('自招核定文號');
+            $table->integer('self_limit')->commect('自招總額');
+            $table->string('scholarship_url')->commect('僑生專屬獎學金詳細說明網址');
+            $table->string('created_at')->commect('');
+            $table->string('updated_at')->commect('');
+            $table->string('deleted_at')->nullable()->commect('');
         });
     }
 
@@ -60,6 +40,10 @@ class CreateSchoolDataTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('school_data');
+        Schema::dropIfExists('school_data')->commect('');
+        $table->string('organization')->comment('學校負責僑生事務的承辦單位');
+        $table->string('dorm_info')->commect('宿舍說明');
+        $table->string('scholarship_info')->commect('獎學金說明');
+        $table->string('scholarship_dept')->commect('獎學金負責單位');
     }
 }
