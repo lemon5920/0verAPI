bff9eb93d62ba6f5200d96c2770d50a0d1f57b79
richegg <zxp86021@gmail.com>
2017-04-24 11:21:16 +0800

    修改 school_data_table 結構

--- database/migrations/2017_03_22_143737_create_school_data_table.php
+++ database/migrations/2017_03_22_143737_create_school_data_table.php
@@ -15,17 +15,31 @@ class CreateSchoolDataTable extends Migration
     {
         Schema::create('school_data', function (Blueprint $table) {
             $table->string('code')->primary()->comment('學校代碼');
+            $table->string('title')->comment('學校名稱');
+            $table->string('engTitle')->comment('學校英文名稱');
+            $table->string('address')->comment('學校地址');
+            $table->string('engAddress')->comment('學校英文地址');
+            $table->string('organization')->comment('學校負責僑生事務的承辦單位名稱');
+            $table->string('engOrganization')->comment('學校負責僑生事務的承辦單位英文名稱');
+            $table->string('dormInfo')->comment('宿舍說明');
+            $table->string('engDormInfo')->comment('宿舍英文說明');
+            $table->string('url')->comment('學校網站網址');
+            $table->string('engUrl')->comment('學校英文網站網址');
             $table->string('type')->comment('「公、私立」與「大學、科大」之組合＋「僑先部」共五種');
             $table->string('phone')->comment('學校聯絡電話（+886-49-2910960#1234）');
             $table->string('fax')->comment('學校聯絡電話（+886-49-2910960#1234）');
             $table->integer('sort_order')->comment('學校顯示排序（教育部給）');
+            $table->boolean('scholarship')->comment('是否提供僑生專屬獎學金');
+            $table->string('scholarshipUrl')->nullable()->comment('僑生專屬獎學金說明網址');
+            $table->string('engScholarshipUrl')->nullable()->comment('僑生專屬獎學金英文說明網址');
+            $table->string('scholarshipDept')->comment('獎學金負責單位名稱');
+            $table->string('engScholarshipDept')->comment('獎學金負責單位英文名稱');
             $table->boolean('five_year_allowed')->comment('[中五]我可以招呢');
             $table->boolean('five_year_prepare')->comment('[中五]我準備招了喔');
             $table->integer('five_year_confirmed_by')->comment('[中五](school code)');
             $table->string('five_year_rule')->comment('[中五]給海聯看的學則');
             $table->string('approve_no')->comment('自招核定文號');
             $table->integer('self_limit')->comment('自招總額');
-            $table->string('scholarship_url')->comment('僑生專屬獎學金詳細說明網址');
             $table->string('created_at');
             $table->string('updated_at');
             $table->string('deleted_at')->nullable();
