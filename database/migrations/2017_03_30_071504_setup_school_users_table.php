df9089eeb9d86dd3725a714a38f6b6b1bd995a80
richegg <zxp86021@gmail.com>
2017-04-25 16:48:11 +0800

    rename migration

--- database/migrations/2017_03_30_071504_setup_school_users_table.php
+++ database/migrations/2017_03_30_071504_setup_school_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SetupUsersTable extends Migration
+class SetupSchoolUsersTable extends Migration
 {
     /**
      * Run the migrations.
