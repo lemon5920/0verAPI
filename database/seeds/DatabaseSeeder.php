81a7d7533da12d683e21e679a8faa27114366384
richegg <zxp86021@gmail.com>
2017-04-26 15:45:03 +0800

    fix typo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(LocalesTableSeeder::class);
+        //$this->call(LocalesTableSeeder::class);
         $this->call(SystemTypesTableSeeder::class);
     }
 }
