32886d812c750e45f067c6019cfd3bdbfab05379
sunnyworm <sunnyworm@gmail.com>
2017-04-27 15:58:59 +0800

    新增研究所系所資料表

---
+++ database/migrations/2017_04_17_160001_setup_graduate_department_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SetupGraduateDepartmentDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('graduate_department_data', function (Blueprint $table) {
+            $table->foreign('school_code')->references('id')->on('school_data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('graduate_department_data', function (Blueprint $table) {
+            $table->dropForeign('department_data_school_code_foreign');
+        });
+    }
+}
