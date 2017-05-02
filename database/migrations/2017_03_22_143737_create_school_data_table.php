8b73c2afab7698ac618d84e95dd5b89c67a73a49
richegg <zxp86021@gmail.com>
2017-05-02 14:41:56 +0800

    更新一下 school_data_table

--- database/migrations/2017_03_22_143737_create_school_data_table.php
+++ database/migrations/2017_03_22_143737_create_school_data_table.php
@@ -32,17 +32,21 @@ class CreateSchoolDataTable extends Migration
             $table->boolean('scholarship')->comment('是否提供僑生專屬獎學金');
             $table->string('scholarship_url')->nullable()->comment('僑生專屬獎學金說明網址');
             $table->string('eng_scholarship_url')->nullable()->comment('僑生專屬獎學金英文說明網址');
-            $table->string('scholarship_dept')->comment('獎學金負責單位名稱');
-            $table->string('eng_scholarship_dept')->comment('獎學金負責單位英文名稱');
+            $table->string('scholarship_dept')->nullable()->comment('獎學金負責單位名稱');
+            $table->string('eng_scholarship_dept')->nullable()->comment('獎學金負責單位英文名稱');
             $table->boolean('five_year_allowed')->comment('[中五]我可以招呢');
             $table->boolean('five_year_prepare')->comment('[中五]我準備招了喔');
-            $table->string('five_year_confirmed_by')->comment('[中五](school code)');
-            $table->string('five_year_rule')->comment('[中五]給海聯看的學則');
+            $table->string('five_year_confirmed_by')->nullable()->comment('[中五](school code)');
+            $table->string('five_year_rule')->nullable()->comment('[中五]給海聯看的學則');
             $table->string('approve_no')->comment('自招核定文號');
             $table->integer('self_limit')->comment('自招總額');
             $table->string('created_at');
             $table->string('updated_at');
             $table->string('deleted_at')->nullable();
+        });
+
+        Schema::table('school_data', function (Blueprint $table) {
+            $table->foreign('five_year_confirmed_by')->references('id')->on('school_data');
         });
     }
 
@@ -53,6 +57,10 @@ class CreateSchoolDataTable extends Migration
      */
     public function down()
     {
+        Schema::table('school_data', function (Blueprint $table) {
+            $table->dropForeign('school_data_five_year_confirmed_by_foreign');
+        });
+
         Schema::dropIfExists('school_data');
     }
 }
