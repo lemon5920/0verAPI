8247f15e4eed21d5fd7e24b3d1adb0844c46d945
richegg <zxp86021@gmail.com>
2017-05-04 11:22:04 +0800

    add SchoolUserController and it's route

---
+++ app/Http/Controllers/SchoolUserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Validator;
+use Carbon\Carbon;
+
+class SchoolUserController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,20 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+
+Route::resource('/schools', 'SchoolDataController');
+
+Route::resource('/users', 'SchoolUserController');
 
 Route::post('/personal-and-priority-data-importer', 'PersonalAndPriorityDataImportController@import');
 
 Route::get('/db-schema-to-md', 'DBSchemaToMDController@export');
 
 Route::get('/limesurvey-filemtime', 'LimesurveyFilemtimeController@export');
-
-Route::resource('/schools', 'SchoolDataController');
