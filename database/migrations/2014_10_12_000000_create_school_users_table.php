30de90830c0601ed1ce97360a2117192db449301
richegg <zxp86021@gmail.com>
2017-05-05 17:16:30 +0800

    修改 school_users_table 結構

--- database/migrations/2014_10_12_000000_create_school_users_table.php
+++ database/migrations/2014_10_12_000000_create_school_users_table.php
@@ -22,7 +22,7 @@ class CreateSchoolUsersTable extends Migration
             $table->string('school_code')->comment('該使用者所屬學校代碼');
             $table->string('organization')->comment('該使用者所屬單位名稱');
             $table->string('phone')->comment('聯絡電話');
-            $table->string('last_login')->comment('上次登入時間 YYYY-MM-DD HH:MM:SS');
+            $table->string('last_login')->nullable()->comment('上次登入時間 YYYY-MM-DD HH:MM:SS');
             $table->string('created_at');
             $table->string('updated_at');
             $table->string('deleted_at')->nullable();
