369818b0b3a33c3728caa3fbfe568d0b697dc0d0
richegg <zxp86021@gmail.com>
2017-05-12 22:34:09 +0800

    add new policy update policy name

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use phpDocumentor\Reflection\Types\Null_;
 
 class UserPolicy
 {
@@ -16,13 +17,19 @@ class UserPolicy
         }
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | admin's
+    |--------------------------------------------------------------------------
+    */
+
     /**
-     * Determine whether the user can view the all admin user list.
+     * Determine whether the user can view admin list.
      *
      * @param  \App\User  $user
      * @return mixed
      */
-    public function list(User $user)
+    public function list_admin(User $user)
     {
         return (bool)$user->admin->admin == true;
     }
@@ -34,7 +41,7 @@ class UserPolicy
      * @param  \App\User  $admin
      * @return mixed
      */
-    public function view(User $user, User $admin)
+    public function view_admin(User $user, User $admin)
     {
         return $user->admin->username === $admin->admin->username;
     }
@@ -45,7 +52,7 @@ class UserPolicy
      * @param  \App\User  $user
      * @return mixed
      */
-    public function create(User $user)
+    public function create_admin(User $user)
     {
         return (bool)$user->admin->admin == true;
     }
@@ -57,7 +64,7 @@ class UserPolicy
      * @param  \App\User  $admin
      * @return mixed
      */
-    public function update(User $user, User $admin)
+    public function update_admin(User $user, User $admin)
     {
         return $user->admin->username === $admin->admin->username;
     }
@@ -68,8 +75,178 @@ class UserPolicy
      * @param  \App\User  $user
      * @return mixed
      */
-    public function delete(User $user)
+    public function delete_admin(User $user)
     {
         return (bool)$user->admin->admin == true;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SchoolEditor
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Determine whether the user can view editor list.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function list_schooleditor(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_editor->admin == true;
+    }
+
+    /**
+     * Determine whether the user can view the editor.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $editor
+     * @return mixed
+     */
+    public function view_schooleditor(User $user, User $editor)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        if ((bool)$user->school_editor->admin == true) {
+            return true;
+        }
+
+        return $user->school_editor->username === $editor->school_editor->username;
+    }
+
+    /**
+     * Determine whether the user can create editors.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create_schooleditor(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_editor->admin == true;
+    }
+
+    /**
+     * Determine whether the user can update the editor.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $editor
+     * @return mixed
+     */
+    public function update_schooleditor(User $user, User $editor)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return $user->school_editor->username === $editor->school_editor->username;
+    }
+
+    /**
+     * Determine whether the user can delete the editor.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function delete_schooleditor(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_editor->admin == true;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SchoolReviewer
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Determine whether the user can view reviewer list.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function list_schoolreviewer(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_reviewer->admin == true;
+    }
+
+    /**
+     * Determine whether the user can view a reviewer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $reviewer
+     * @return mixed
+     */
+    public function view_schoolreviewer(User $user, User $reviewer)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return $user->school_reviewer->username === $reviewer->school_reviewer->username;
+    }
+
+    /**
+     * Determine whether the user can create reviewers.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create_schoolreviewer(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_reviewer->admin == true;
+    }
+
+    /**
+     * Determine whether the user can update a reviewer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $reviewer
+     * @return mixed
+     */
+    public function update_schoolreviewer(User $user, User $reviewer)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return $user->school_reviewer->username === $reviewer->school_reviewer->username;
+    }
+
+    /**
+     * Determine whether the user can delete a reviewer.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function delete_schoolreviewer(User $user)
+    {
+        if ($user->admin != NULL) {
+            return true;
+        }
+
+        return (bool)$user->school_reviewer->admin == true;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,14 +25,6 @@ Route::post('/users/login', 'Auth\LoginController@UserLogin');
 
 Route::post('/users/logout', 'Auth\LoginController@UserLogout');
 
-//Route::post('/editors/login', 'Auth\SchoolEditor\LoginController@SchoolEditorLogin');
-
-//Route::post('/editors/logout', 'Auth\SchoolEditor\LoginController@SchoolEditorLogout');
-
-//Route::post('/admins/login', 'Auth\Admin\LoginController@AdminLogin');
-
-//Route::post('/admins/logout', 'Auth\Admin\LoginController@AdminLogout');
-
 Route::resource('/admins', 'AdminController');
 
 Route::resource('/schools', 'SchoolDataController');
